185932ae04fd6d4d59273c5b5bb7c7102a1f1118
Charlotte <smit-charlotte@outlook.com>
2025-05-27 11:26:03 +0200

    update homepage

--- src/assets/components/nav/Nav.jsx
+++ src/assets/components/nav/Nav.jsx
@@ -1,13 +1,28 @@
 import { Link } from 'react-router-dom'
 import './nav.css'
+import logo from "../../../../templates/assets/shared/logo.svg"
 
 export default function Nav() {
 
 
     return(
 
-
         <>
+
+        <nav className="d-flex gap-4">
+            <div className="nav-logo d-flex align-items-center">
+                <img src={logo} alt="" />
+            </div>
+            <div className="nav-ligne">
+                <div className="ligne"></div>
+            </div>
+            <div className="nav-menu d-flex align-items-center justify-content-center gap-5">
+                <p>00 HOME</p>
+                <p>01 DESTINATION</p>
+                <p>02 CREW</p>
+                <p>03 TECHNOLOGY</p>
+            </div>
+        </nav>
         
         </>
     )
