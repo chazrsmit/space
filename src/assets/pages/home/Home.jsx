588354db3bd81e86f312c794fa05e5b142ee4662
Charlotte <smit-charlotte@outlook.com>
2025-05-27 14:07:30 +0200

    update

--- src/assets/pages/home/Home.jsx
+++ src/assets/pages/home/Home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
                             <h1>SPACE</h1>
                         </div>
                         <div>
-                            <p className="w-75">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+                            <p className="para">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                         </div>
                     </div>
                     <div className="home-2  d-flex justify-content-end align-items-center">
