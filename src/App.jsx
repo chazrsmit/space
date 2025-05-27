894b4c2099335e6d530f359ef2be061011ceb627
Charlotte <smit-charlotte@outlook.com>
2025-05-27 12:22:34 +0200

    update link menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './assets/pages/home/Home'
-
+import Crew from './assets/pages/crew/Crew'
+import Tech from './assets/pages/tech/Tech'
+import Destination from './assets/pages/destination/Destination'
 
 function App() {
 
@@ -11,6 +13,9 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/tech" element={<Tech />} />
       </Routes>
     </>
   )
--- src/assets/components/nav/Nav.jsx
+++ src/assets/components/nav/Nav.jsx
@@ -17,10 +17,18 @@ export default function Nav() {
                 <div className="ligne"></div>
             </div>
             <div className="nav-menu d-flex align-items-center justify-content-center gap-5">
-                <p>00 HOME</p>
-                <p>01 DESTINATION</p>
-                <p>02 CREW</p>
-                <p>03 TECHNOLOGY</p>
+                <div className="link1" aria-current>
+                    <p><Link to="/">00 HOME</Link></p>
+                </div>
+                <div>
+                    <p><Link to ="/destination">01 DESTINATION</Link></p>
+                </div>
+                <div>
+                    <p><Link to="/crew">02 CREW</Link></p>
+                </div>
+                <div>
+                    <p><Link to="/tech">03 TECHNOLOGY</Link></p>
+                </div>
             </div>
         </nav>
         
